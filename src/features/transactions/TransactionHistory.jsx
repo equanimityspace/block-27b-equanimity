1e05263ea9f0f86417248fc89b9725f1c5ec3076
Ryan <ryfly2016@gmail.com>
2025-04-07 10:28:39 -0600

    finish transactionHistory again

--- src/features/transactions/TransactionHistory.jsx
+++ src/features/transactions/TransactionHistory.jsx
@@ -1,4 +1,5 @@
 import "./transactionHistory.scss";
+import { useSelector } from "react-redux";
 
 /** Displays a table row with transaction information  */
 const TransactionRow = ({ transaction: { type, amount, balance } }) => (
@@ -12,8 +13,8 @@ const TransactionRow = ({ transaction: { type, amount, balance } }) => (
 /** Displays a table of the user's transaction history. */
 export default function TransactionHistory() {
   // TODO: Get the transaction history from the Redux store using the useSelector hook
-  const history = [];
-
+  // const history = [];
+  const history = useSelector((state) => state.transactions.history);
   return (
     <section className="transactions-history container">
       <h2>Transaction History</h2>
@@ -26,10 +27,15 @@ export default function TransactionHistory() {
           </tr>
         </thead>
         <tbody>
-          {/* TODO
-          Map over the transactions in `history`
-          to render the appropriate `TransactionRow`s
-          */}
+          {history.map((item) => {
+            return (
+              <tr key={history.indexOf(item)}>
+                <td>{item.type}</td>
+                <td>{item.amount}</td>
+                <td>{item.balance}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </section>
